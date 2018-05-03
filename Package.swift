bd4ac7e47895d748282265373e38b0d99c2b9c78
Hulk <hulk104255@gmail.com>
2018-05-03 23:11:36 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,9 +3,8 @@ import PackageDescription
 let package = Package(
     name: "HTTPServer",
     dependencies: [
-        .Package(url: "https://github.com/yoshiki/Curl.git", majorVersion: 0),
-        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 12),
-        .Package(url: "https://github.com/hulk10425/HTTPServer.git", majorVersion: 0, minor: 13),
-        .Package(url: "https://github.com/ZewoGraveyard/Base64.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/Zewo/HTTPFile.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/hulk10425/TCP.git", majorVersion: 0, minor: 14),
     ]
 )
